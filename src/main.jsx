501ec5f6bf0fb355f5b3288606bf8204f7beb7e8
angiediaz0209 <steff.diaz0209@gmail.com>
2026-02-12 19:51:10 -0800

    Clean up routing fixes

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-// Handle GitHub Pages redirect
-(function() {
-  var redirect = sessionStorage.redirect;
-  delete sessionStorage.redirect;
-  if (redirect && redirect !== location.href) {
-    history.replaceState(null, null, redirect.replace('https://angiediaz0209.github.io/artistline', ''));
-  }
-})();
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
